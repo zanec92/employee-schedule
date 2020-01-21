917f429b03fd49d11bfde19e48f278d927999bba
developer <developer@bloknot.desktop>
2020-01-21 14:01:07 +0300

    schedule and vacation models preparation

--- app/Schedule.php
+++ app/Schedule.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Schedule extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        'start',
+        'end'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     * 
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
 }
--- app/Vacation.php
+++ app/Vacation.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vacation extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        'vacation_from',
+        'vacation_to'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     * 
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'vacation_from',
+        'vacation_to'
+    ];
 }
